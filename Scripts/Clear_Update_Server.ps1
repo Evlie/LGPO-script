cd62ba773c14640012e445a6d8176890e507c632
Evlie <138787573+Evlie@users.noreply.github.com>
2025-09-10 13:23:20 +0100

    Adding Ps1 script
    Clears windows target update server policy

---
+++ Scripts/Clear_Update_Server.ps1
@@ -0,0 +1,21 @@
+
+
+#Intial folder and files downloads
+$baseDir = [System.IO.Path]::GetTempPath() + "MNB-RMM-TOOLS"
+
+if (!(Test-Path $baseDir)) {
+    mkdir $baseDir
+}
+if (!(Test-Path $baseDir/Policies)) {
+    mkdir $baseDir/Policies
+}
+
+Invoke-WebRequest -Uri "https://github.com/Evlie/LGPO-script/raw/refs/heads/main/LGPO.exe" -OutFile "$baseDir/LGPO.exe"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Evlie/LGPO-script/refs/heads/main/Policies/Remove_Target_Update_Server.txt" -OutFile "$baseDir/Policies/Remove_Target_Update_Server.scp"
+
+
+
+#Begin script execution 
+& "$baseDir/LGPO.exe" /t $baseDir/Policies/Remove_Target_Update_Server.scp
+clear
+Remove-Item $baseDir -Recurse -Force
