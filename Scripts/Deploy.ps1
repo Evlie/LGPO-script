c695d562a07848bfac600e77f1069fb9429be992
Evlie <138787573+Evlie@users.noreply.github.com>
2025-09-25 11:39:27 +0100

    Update Deploy.ps1

--- Scripts/Deploy.ps1
+++ Scripts/Deploy.ps1
@@ -63,10 +63,8 @@ foreach ($job in $jobs) {
 
 
 if ($adobe) {
-    if (!$devTest -or !(Test-Path "$baseDir/AcroRdrDC.exe")) {Invoke-WebRequest -Uri $adobeMSI -OutFile "$baseDir/AcroRdrDC.exe"} 
     Start-Process -FilePath "$baseDir/AcroRdrDC.exe" -ArgumentList "/sAll /rs EULA_ACCEPT=YES" -Wait
     Write-Output("Adobe install complete.")
-    
 
     if (!(Test-Path -LiteralPath $adobeReaderReg)) { [void] (New-Item -Path $adobeReaderReg -Force) }
     if (!(Test-Path -LiteralPath $adobeAcrobatReg)) { [void] (New-Item -Path $adobeAcrobatReg -Force) }
@@ -75,23 +73,16 @@ if ($adobe) {
 }
 
 if ($chrome) {
-    if (!$devTest -or !(Test-Path "$baseDir/ChromeInstaller.msi")) {Invoke-WebRequest -Uri $chromeMSI -OutFile "$baseDir/ChromeInstaller.msi"}
     Start-Process -FilePath "$baseDir/ChromeInstaller.msi" -ArgumentList "/qn" -Wait
     Write-Output("Chrome install complete.")
 }
 
 if ($firefox) {
-    if (!$devTest -or !(Test-Path "$baseDir/FirefoxInstaller.msi")) {Invoke-WebRequest -Uri $firefoxMSI -OutFile "$baseDir/FirefoxInstaller.msi"}
     Start-Process -FilePath "$baseDir/FirefoxInstaller.msi" -ArgumentList "/quiet" -Wait
     Write-Output("Firefox install complete.")
 }
 
 if ($office) {
-    if (!(Test-Path "$baseDir/Office")) { mkdir "$baseDir/Office" }
-    if (!$devTest -or !(Test-Path "$baseDir/Office/OfficeSetup.exe")) { Invoke-WebRequest -Uri "https://github.com/Evlie/LGPO-script/raw/refs/heads/main/Office/setup.exe" -OutFile "$baseDir/Office/OfficeSetup.exe" }
-    if (Test-Path "$baseDir/Office/Default_apps_for_business.xml") { Remove-Item -Path "$baseDir/Office/Default_apps_for_business.xml" }
-    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Evlie/LGPO-script/refs/heads/main/Office/Default_apps_for_business.xml" -OutFile "$baseDir/Office/Default_apps_for_business.xml"
-    
     Start-Process -FilePath "$baseDir/Office/OfficeSetup.exe" -ArgumentList "/download "$baseDir/Office/Default_apps_for_business.xml"" -Wait
     Start-Process -FilePath "$baseDir/Office/OfficeSetup.exe" -ArgumentList "/configure "$baseDir/Office/Default_apps_for_business.xml"" -Wait
 
